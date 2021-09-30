9a241d381c796dfae4887a61d45d5642ca7aaa4f
ADobrynin <adobrynin@t1-consulting.ru>
2021-09-30 10:31:13 +0300

    9/9 Streams.kt

--- streams/src/main/kotlin/ru/sber/streams/Streams.kt
+++ streams/src/main/kotlin/ru/sber/streams/Streams.kt
@@ -2,18 +2,16 @@ package ru.sber.streams
 
 
 // 1. Используя withIndex() посчитать сумму элементов листа, индекс которых кратен 3. (нулевой индекс тоже входит)
-fun getSumWithIndexDivisibleByThree(list: List<Long>): Long {
-    return list.withIndex()
-        .filter { it.index % 3 == 0 }
-        .sumOf { it.value }
+fun getSumWithIndexDivisibleByThree(list: List<Long>): Long = list.withIndex()
+    .filter { it.index % 3 == 0 }
+    .sumOf { it.value }
 
-}
 
 // 2. Используя функцию generateSequence() создать последовательность, возвращающую числа Фибоначчи.
-fun generateFibonacciSequence(): Sequence<Int> {
-    return generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }
+fun generateFibonacciSequence(): Sequence<Int> =
+    generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }
         .map { it.first }
-}
+
 
 // 3. Получить города, в которых есть покупатели.
 fun Shop.getCustomersCities(): Set<City> = customers.asSequence()
@@ -37,28 +35,31 @@ fun Customer.getMostExpensiveProduct(): Product? = orders.asSequence()
 
 // 7. Получить соответствие в мапе: город - количество заказанных и доставленных продуктов в данный город.
 
-fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> =
-    customers.map { it.city }
-        .distinct()
-        .associateWith { city ->
-            customers.filter { it.city == city } // получили всех кастомеров для определенного города
+fun Shop.getNumberOfDeliveredProductByCity(): Map<City, Int> = customers.asSequence()
+    .map { it.city }
+    .distinct()
+    .associateWith { city ->
+        customers.filter { it.city == city } // получили всех кастомеров для определенного города
             .flatMap { order -> order.orders.filter { it.isDelivered } } // убираем недоставленные заказы
             .flatMap { it.products } // получаем списк покупок
-            .count() }
+            .count()
+    }
 
 // 8. Получить соответствие в мапе: город - самый популярный продукт в городе.
-fun Shop.getMostPopularProductInCity(): Map<City, Product> = emptyMap()
-//    customers.map { it.city }
-//        .distinct()
-//        .associateWith {  city ->
-//            customers.filter { it.city == city }
-//                .flatMap { order -> order.orders.flatMap { it.products } }
-//            }
-//        .mapValues {  elem ->
-//            elem.value.count
-//        }
-
+fun Shop.getMostPopularProductInCity(): Map<City, Product> = customers
+    .groupBy { it.city }
+    .mapValues { entry ->
+        entry.value
+            .flatMap { it.orders }
+            .flatMap { it.products }
+            .groupingBy { it }
+            .eachCount()
+    }
+    .map { entry -> entry.key to entry.value.maxByOrNull { it.value }!!.key }
+    .toMap()
 
 
 // 9. Получить набор товаров, которые заказывали все покупатели.
-fun Shop.getProductsOrderedByAll(): Set<Product> = emptySet()
+fun Shop.getProductsOrderedByAll(): Set<Product> = customers.asSequence()
+    .map { it.orders.flatMap { order -> order.products }.toSet() }
+    .reduce { acc: Set<Product>, set: Set<Product> -> acc.intersect(set) }
