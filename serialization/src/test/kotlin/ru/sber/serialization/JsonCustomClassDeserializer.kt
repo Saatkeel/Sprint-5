2d4701d2f0da08dba4bf745fe5e7dd40c1572049
ADobrynin <adobrynin@t1-consulting.ru>
2021-10-02 21:30:05 +0300

    serialization 4/4 first try

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,9 +1,19 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
+import java.io.IOException
 import kotlin.test.assertEquals
+
 
 class JsonCustomClassDeserializer {
 
@@ -11,7 +21,15 @@ class JsonCustomClassDeserializer {
     fun `Нобходимо десериализовать данные в класс`() {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
+
+        val module = SimpleModule()
+        module.addDeserializer(Client7::class.java, ItemDeserializer())
         val objectMapper = ObjectMapper()
+            .registerModules(
+                KotlinModule(),
+                JavaTimeModule(),
+                module
+            )
 
         // when
         val client = objectMapper.readValue<Client7>(data)
@@ -21,4 +39,23 @@ class JsonCustomClassDeserializer {
         assertEquals("Иванов", client.lastName)
         assertEquals("Иванович", client.middleName)
     }
+}
+
+class ItemDeserializer : StdDeserializer<Client7?>(Client7::class.java) {
+
+    @Throws(IOException::class, JsonProcessingException::class)
+    override fun deserialize(jp: JsonParser, ctxt: DeserializationContext?): Client7 {
+
+        val node = jp.codec.readTree<JsonNode>(jp)
+
+        val client = node.get("client").asText().split(" ")
+
+        return Client7(
+            client[1],//lastname
+            client[0],//firstname
+            client[2]//middlename
+        )
+
+    }
+
 }
